02f2a2b6e4841a4433151c6346078edf44668606
vicky-0210 <vivekgupta8106@gmail.com>
2025-02-22 14:51:09 +0530

    made some changes

--- frontend/src/components/Dashboard/contents.jsx
+++ frontend/src/components/Dashboard/contents.jsx
@@ -1,18 +1,15 @@
 import { Link } from "react-router-dom";
-import { FaPlusCircle, FaUsers, FaHistory, FaShareAlt } from "react-icons/fa";
+import { FaPlusCircle, FaSearch } from "react-icons/fa";
 
 const DashboardGrid = () => {
   const options = [
-    { name: "Add Capsule", icon: <FaPlusCircle />, link: "/add-capsule" },
-    { name: "Collaborate", icon: <FaUsers />, link: "/collaborate" },
-    { name: "History", icon: <FaHistory />, link: "/history" },
-    { name: "Share", icon: <FaShareAlt />, link: "/share" },
+    { name: "Add Capsule", icon: <FaPlusCircle />, link: "create-capsule" },
+    { name: "Explore Capsules", icon: <FaSearch />, link: "explore-capsules" },
   ];
 
   return (
     <div className="p-6">
-      <h2 className="text-2xl font-semibold text-center mb-6">Dashboard</h2>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+      <div className="grid grid-cols-2 gap-6">
         {options.map((option, index) => (
           <Link
             to={option.link}
